a4371d1b4799da302c115402c52a70140b3711a8
AbdurMohammed <54000467+AbdurMohammed@users.noreply.github.com>
2020-11-01 23:03:50 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './rrlogo2.png'
 import './App.css';
 
 function App() {
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <h1>Awsome Zuhu is stream</h1>
+    
+       <h1>Awsome Zuhu likes to stream stream</h1>
       </header>
     </div>
   );
